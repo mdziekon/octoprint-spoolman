4934c1402d7f8b82c13a71652c0fdc8835ff4eba
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2024-03-21 02:55:54 +0100

    GH-8 | Add feedback popup
    Signed-off-by: Michał Dziekoński <michal.dziekonski+github@gmail.com>

--- octoprint_Spoolman/static/js/common/octoprint.js
+++ octoprint_Spoolman/static/js/common/octoprint.js
@@ -7,3 +7,54 @@ async function reloadSettingsViewModel(viewModel) {
 
     return settingsSavePromise;
 };
+
+/**
+ * @typedef {object} ShowPopupParams
+ * @property {string} type
+ * @property {string} subject
+ * @property {string} message
+ * @property {boolean} shouldDeduplicate
+ * @property {boolean} shouldAutoclose
+ * @property {boolean} shouldShowType
+ */
+
+/**
+ * @param {ShowPopupParams} params
+ */
+function showSpoolmanPopup(params) {
+    const {
+        type,
+        subject,
+        message,
+        shouldDeduplicate = false,
+        shouldAutoclose,
+        shouldShowType = false,
+    } = params;
+
+    const title = (() => {
+        const baseTitle = `Spoolman: ${subject}`;
+        const prefix = (
+            shouldShowType
+                ? `[${type.toUpperCase()}] `
+                : ''
+        );
+
+        return `${prefix}${baseTitle}`;
+    })();
+
+    const popupIdClass = `${title}-${message}`.replace(/([^a-z0-9]+)/gi, '-');
+
+    const hasSamePopupAlready = document.querySelector(`.${popupIdClass}`) !== null;
+
+    if (shouldDeduplicate && hasSamePopupAlready) {
+        return;
+    }
+
+    new PNotify({
+        title,
+        text: message.trim(),
+        type,
+        hide: shouldAutoclose,
+        addclass: popupIdClass
+    });
+}
