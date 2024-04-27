e88e76ac5af516f87ace1cc4c9b36ccd70b986ef
Michal Dziekonski (mdz) <michal.dziekonski+github@gmail.com>
2024-04-27 20:19:52 +0200

    GH-4 | Add missing setup files (#24)
    * GH-4 | Add missing setup files
    
    Signed-off-by: Michał Dziekoński <michal.dziekonski+github@gmail.com>
    
    * GH-4 | Use cookiecutter to prepare for release
    
    Signed-off-by: Michał Dziekoński <michal.dziekonski+github@gmail.com>
    
    ---------
    
    Signed-off-by: Michał Dziekoński <michal.dziekonski+github@gmail.com>

--- octoprint_Spoolman/__init__.py
+++ octoprint_Spoolman/__init__.py
@@ -4,16 +4,17 @@ from __future__ import absolute_import
 from .SpoolmanPlugin import SpoolmanPlugin
 
 __plugin_name__ = "Spoolman"
-__plugin_version__ = "0.1.0-dev.1"
+__plugin_version__ = "1.0.0"
 __plugin_description__ = "Plugin integrating OctoPrint with Spoolman, a universal filament spools inventory manager."
 __plugin_pythoncompat__ = ">=3.7,<4"
 
 def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = SpoolmanPlugin()
+    global __plugin_implementation__
+    __plugin_implementation__ = SpoolmanPlugin()
 
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.comm.protocol.gcode.sent": __plugin_implementation__.on_sentGCodeHook,
-		"octoprint.events.register_custom_events": __plugin_implementation__.register_custom_events,
-	}
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
+        "octoprint.comm.protocol.gcode.sent": __plugin_implementation__.on_sentGCodeHook,
+        "octoprint.events.register_custom_events": __plugin_implementation__.register_custom_events,
+    }
