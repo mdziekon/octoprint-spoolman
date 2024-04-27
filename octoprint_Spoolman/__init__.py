485728bf20616d4a76ecc23e2c5ba50c6ab21931
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2024-04-27 22:05:14 +0200

    GH-0 | Release v1.0.1

--- octoprint_Spoolman/__init__.py
+++ octoprint_Spoolman/__init__.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 from .SpoolmanPlugin import SpoolmanPlugin
 
 __plugin_name__ = "Spoolman"
-__plugin_version__ = "1.0.0"
+__plugin_version__ = "1.0.1"
 __plugin_description__ = "Plugin integrating OctoPrint with Spoolman, a universal filament spools inventory manager."
 __plugin_pythoncompat__ = ">=3.7,<4"
 
