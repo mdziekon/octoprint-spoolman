ef9de758e4cf89897f807b0145aa3bf7cba1b1e6
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2024-05-01 20:43:40 +0200

    chore: release 1.1.0
    Signed-off-by: Michał Dziekoński <michal.dziekonski+github@gmail.com>

--- octoprint_Spoolman/__init__.py
+++ octoprint_Spoolman/__init__.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 from .SpoolmanPlugin import SpoolmanPlugin
 
 __plugin_name__ = "Spoolman"
-__plugin_version__ = "1.1.0rc1"
+__plugin_version__ = "1.1.0"
 __plugin_description__ = "Plugin integrating OctoPrint with Spoolman, a universal filament spools inventory manager."
 __plugin_pythoncompat__ = ">=3.7,<4"
 
